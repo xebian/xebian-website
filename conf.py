a931e8c473d216dc5840dbf06b7c66704838e510
Unit 193 <unit193@ubuntu.com>
2016-04-13 10:33:46 -0400

    Don't add a mailto link.

--- conf.py
+++ conf.py
@@ -457,7 +457,7 @@ LICENSE = ""
 
 # A small copyright notice for the page footer (in HTML).
 # (translatable)
-CONTENT_FOOTER = 'Contents &copy; {date} <a href="mailto:{email}">{author}</a> &#8211; Powered by <a href="http://getnikola.com" rel="nofollow">Nikola</a> {license}'
+CONTENT_FOOTER = 'Contents &copy; {date} {author} &#8211; Powered by <a href="http://getnikola.com" rel="nofollow">Nikola</a> {license}'
 
 # Things that will be passed to CONTENT_FOOTER.format().  This is done
 # for translatability, as dicts are not formattable.  Nikola will
